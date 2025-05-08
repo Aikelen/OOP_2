f5c282aa028b3946a97ef97cfb6e8beb2b9cb6ce
Айкелен <Aikelen.030904@gmail.com>
2025-05-08 22:48:47 +0300

    delete methods

--- src/ServiceStation.java
+++ src/ServiceStation.java
@@ -1,24 +1,5 @@
 class ServiceStation {
-    /**
-     * Полиморфный метод: принимает любой транспорт, у которого реализован Vehicle.
-     */
     public void check(Vehicle vehicle) {
         vehicle.service();
-    }
-
-    /*
-     * Перегрузка: по требованиям домашки добавим отдельные входы,
-     * но внутри всё равно делегируем на общий метод.
-     */
-    public void check(Car car) {
-        check((Vehicle) car);
-    }
-
-    public void check(Truck truck) {
-        check((Vehicle) truck);
-    }
-
-    public void check(Bicycle bicycle) {
-        check((Vehicle) bicycle);
     }
 }
